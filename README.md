263787da8490bb3d16b6d8afce7704d46cd469d2
Alexander Pataridze <s.pataridze@gmail.com>
2021-11-26 15:58:54 +0400

    feat: add rename script

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+## ExodusMovement/country-flag-icons
+Flags 3 Characters
+
+
 This is just free hosting of [`country-flag-icons`](https://gitlab.com/catamphetamine/country-flag-icons).
---
+++ script.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const path = "./3x2/";
+const alphaMap = require("./alpha2-alpha3-map.json");
+
+const filesArray = fs
+  .readdirSync(path)
+  .filter((file) => fs.lstatSync(path + file).isFile());
+
+alphaMap.forEach((map) => {
+  for (let i = 0; i < filesArray.length; i++) {
+    const file = filesArray[i];
+    if (file.replace(".svg", "") === map[0]) {
+      fs.renameSync(path + file, path + map[1] + ".svg");
+      break;
+    }
+  }
+});
+
+// console.log(filesArray);
